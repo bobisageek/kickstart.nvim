513b1b4961133dd897c6171cb2ac14dc3293c2ca
Bob <highpressurecarsalesman@gmail.com>
2025-10-11 10:31:56 -0400

    - add parinfer - autopair more - add fidget as telescope plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -24,6 +24,7 @@ return {
       },
     }
     require('telescope').load_extension 'fzy_native'
+    require('telescope').load_extension 'fidget'
 
     -- -- Enable Telescope extensions if they are installed
     pcall(require('telescope').load_extension, 'ui-select')
