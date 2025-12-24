d9ec975041d86f87505ffef408cc07dcf65c5c9c
Bob <highpressurecarsalesman@gmail.com>
2025-12-24 01:06:05 -0500

    rip out lspconfig stuff

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,8 +6,6 @@ return {
     -- Mason must be loaded before its dependents so we need to set it up here.
     -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
     { 'williamboman/mason.nvim', opts = {} },
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
 
     -- Useful status updates for LSP.
     { 'j-hui/fidget.nvim', opts = {} },
@@ -165,12 +163,6 @@ return {
       -- pyright = {},
       rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
 
       lua_ls = {
         -- cmd = { ... },
@@ -181,48 +173,16 @@ return {
             completion = {
               callSnippet = 'Replace',
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
+            runtime = { version = 'LuaJIT' },
           },
         },
       },
+      nushell = { cmd = { 'nu', '-n', '--lsp' } },
     }
 
-    -- Ensure the servers and tools above are installed
-    --
-    -- To check the current status of installed tools and/or manually install
-    -- other tools, you can run
-    --    :Mason
-    --
-    -- You can press `g?` for help in this menu.
-    --
-    -- `mason` had to be setup earlier: to configure its options see the
-    -- `dependencies` table for `nvim-lspconfig` above.
-    --
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-    })
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-    require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
-      automatic_enable = true,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          vim.lsp.config(server_name).setup(server)
-        end,
-      },
-    }
-    vim.lsp.config('nushell', { cmd = { 'nu', '-n', '--lsp' } })
-    vim.lsp.enable 'nushell'
+    for server_name, server in pairs(servers) do
+      vim.lsp.config(server_name, server)
+      vim.lsp.enable(server_name)
+    end
   end,
 }
